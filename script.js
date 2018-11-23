7e392219812510146848e163c5e27af772c18b8e
KAKAO <kakao@KAKAOui-MacBook-Pro-24.local>
2018-11-23 15:08:04 +0900

    dd
    re

--- script.js
+++ script.js
@@ -9,12 +9,14 @@ anime.easings['myCustomEasingName'] = function(t){
 for (i = 0; i < windowWidth/2; i++) { 
   $( ".wrapper" ).append(addedList);
   $(".child").eq(i).css('left',2*listCount--);
+}
 
+for (i = 0; i < windowWidth/2; i++) { 
   anime({
-    targets: 'li:nth-child('+i+')',
+    targets: 'li:nth-child('+i+') .child img',
     translateY:[
         function(){ return anime.random(-5, 5) +'vh';}, 
-        function(){ return anime.random(-70, 70) +'vh';} 
+        function(){ return anime.random(-75, 75) +'vh';} 
     ],
     duration: function(){ return anime.random(1000, 5000)},
     loop: true,
@@ -25,13 +27,30 @@ for (i = 0; i < windowWidth/2; i++) {
 
 $('.wrapper').click(function(){
   anime({
-    targets: 'li',
-    translateY: [function(){ return anime.random(-70, 70) +'vh';}, 0],
+    targets: 'li .child img',
+    translateY: 0,
     duration: 1000,
+    direction: 'alternate',
     loop: false,
     elasticity: 10000,
   });
+  setTimeout(function(){
+    for (i = 0; i < windowWidth/2; i++){ 
+      anime({
+        targets: 'li:nth-child('+i+') .child img',
+        translateY:[
+            0, 
+            function(){ return anime.random(-75, 75) +'vh';} 
+        ],
+        duration: function(){ return anime.random(1000, 5000)},
+        loop: true,
+        elasticity: 5000,
+        easing: 'myCustomEasingName',
+      });
+    }
+  },700);
 });
+
 
 var html = document.documentElement;
 var body = document.body;
