6227d6757ed5833ad7685919a11fe576069b9b70
KAKAO <kakao@KAKAOui-MacBook-Pro-117.local>
2019-01-29 22:48:48 +0900

    ddd

---
+++ js/postersecond.js
@@ -0,0 +1,7 @@
+const posterSecond = lottie.loadAnimation({
+      container: $('#item2 .poster'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: false,
+      path: 'data.json'
+    });
