c70376f8a40d9ed967d4592d3c23a67f71212302
cathy03 <yoonyeojooncathy@gmail.com>
2023-10-14 14:20:03 +0900

    Delete js directory

--- js/postersecond.js
+++
@@ -1,7 +0,0 @@
-const posterSecond = lottie.loadAnimation({
-      container: document.getElementById('poster-second'),
-      renderer: 'svg',
-      loop: true,
-      autoplay: false,
-      path: 'data.json'
-    });
